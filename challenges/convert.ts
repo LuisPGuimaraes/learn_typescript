543efa05e80388dfb242b8a76c182716c3858bae
LuisPGuimaraes <luisegsilva.le@gmail.com>
2025-03-17 23:03:23 -0300

    add convert and sum functions in TypeScript with example usage

---
+++ challenges/convert.ts
@@ -0,0 +1,8 @@
+const myNumber: number = 5;
+const converted_number: string = convert(myNumber);
+
+function convert(num: number): string {
+  return num.toString();
+}
+
+console.log(converted_number);
